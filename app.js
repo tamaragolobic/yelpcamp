5f6cce1a11a0f30348595c462603ee28e3152503
Tamara Golobic <tamara.golobic@gmail.com>
2020-09-06 12:05:57 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,116 @@
+let Campground = require("./models/campgrounds"),
+    Comment    = require("./models/comment"),
+    //User       = require("./models/user"),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose"),
+    express    = require("express"),
+    seedsDB    = require("./seeds"),
+    app        = express();
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+seedsDB();
+
+let campgrounds = [
+    {
+        name: "Salmon Creek",
+        image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZUDDu_Fjst-xTYZlHG4TgX36epjNZafl36Q&usqp=CAU"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJ79So02YkPxgsLnxxQ14u55xauvn0Ul0j1A&usqp=CAU"
+    },
+    {
+        name: "Salmon Creek",
+        image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZUDDu_Fjst-xTYZlHG4TgX36epjNZafl36Q&usqp=CAU"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJ79So02YkPxgsLnxxQ14u55xauvn0Ul0j1A&usqp=CAU"
+    }
+];
+
+app.get("/", function (req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function (req, res) {
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds/campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+})
+
+app.post("/campgrounds", function (req, res) {
+    Campground.create(req.body.campground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", function (req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+       if(err) {
+           console.log(err);
+       }
+       else {
+           res.render("comments/new", {campground: campground});
+       }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+       if(err) {
+           console.log(err);
+           res.redirect("/campgrounds");
+       }
+       else {
+           Comment.create(req.body.comment, function(err, comment) {
+              if(err) {
+                  console.log(err);
+              }
+              else {
+                  campground.comments.push(comment);
+                  campground.save();
+                  res.redirect("/campgrounds/" + campground._id);
+              }
+           });
+       }
+    });
+});
+
+app.listen(3000, function () {
+    console.log("The YelpCamp Server Has Started");
+});
---
+++ seeds.js
@@ -0,0 +1,56 @@
+let Campground = require("./models/campgrounds"),
+    Comment    = require("./models/comment"),
+    mongoose   = require("mongoose");
+
+let data = [
+        {
+            name: "Cloud's Rest",
+            image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident,"
+        },
+        {
+            name: "Desert Masa",
+            image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident,"
+        },
+        {
+            name: "Canyon Floor",
+            image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9",
+            description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident,"
+        }
+    ];
+
+function seedDB() {
+    Campground.remove({}, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Removed campground");
+            data.forEach(function (seed) {
+                Campground.create(seed, function (err, campground) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Added a campground");
+                        Comment.create(
+                            {
+                                text: "Yes it is",
+                                author: "Tamara"
+                            },
+                            function (err, comment) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        }
+    });
+}
+
+module.exports = seedDB;
